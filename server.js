de817e5c0faae496a18c27fc26bdafe3ac02308d
Ankit <deva80657@gmail.com>
2022-03-29 09:24:34 +0530

    fix some issue

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+//const PORT = process.env.PORT || 5000;
+server.listen(process.env.PORT || 5000, () => {
+  console.log(`Listening on port ${process.env.PORT || 5000}`);
 });
